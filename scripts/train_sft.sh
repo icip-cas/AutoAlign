43a24c0a2cea4c6b3874a01c6be3812e0d2f691d
guanxinyan <guanxinyan2022@iscas.ac.cn>
2024-07-03 13:20:13 +0800

    upload training scripts

--- scripts/train_sft.sh
+++ scripts/train_sft.sh
@@ -5,7 +5,7 @@ torchrun --nproc_per_node="${N_GPUS:-"8"}" \
     --node_rank="${RANK:-"0"}" \
     --master_port="${MASTER_PORT:-"2333"}" src/autoalign/train/sft.py \
     --model_name_or_path ${MODEL_PATH:-"Qwen2/Qwen2-7B"}  \
-    --data_path ${DATA_PATH:-"data/dummy_conversation.json"} \
+    --data_path ${DATA_PATH:-"data/dummy_sft.json"} \
     --conv_template_name ${CONV_TEMPLATE:-"qwen-7b-chat"} \
     --bf16 True \
     --output_dir ${OUTPUT_DIR:-"models/qwen2-7b"} \
