9be573969720cb7832fcfd7d3b7627b057a0179f
guanxinyan <guanxinyan2022@iscas.ac.cn>
2024-07-05 11:21:22 +0800

    support mistral template

--- scripts/train_dpo.sh
+++ scripts/train_dpo.sh
@@ -6,7 +6,7 @@ torchrun --nproc_per_node="${N_GPUS:-"8"}" \
     --node_rank="${RANK:-"0"}" \
     --master_port="${MASTER_PORT:-"2333"}" src/autoalign/train/dpo.py \
     --model_name_or_path ${MODEL_PATH:-"Qwen2/Qwen2-7B"}  \
-    --data_path ${DATA_PATH:-"data/dummy_dpo.jsonl"} \
+    --data_path ${DATA_PATH:-"data/dummy_dpo.json"} \
     --conv_template_name ${CONV_TEMPLATE:-"qwen-7b-chat"} \
     --bf16 True \
     --output_dir ${OUTPUT_DIR:-"models/qwen2-7b"} \
