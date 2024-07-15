ed983d0608cf5c9fedd3867824fcbd112adaece8
guanxinyan <guanxinyan2022@iscas.ac.cn>
2024-07-15 19:16:29 +0800

    updata get ultrafeedback

--- tools/get_ultrafeedback.py
+++ tools/get_ultrafeedback.py
@@ -1,6 +1,15 @@
 from datasets import load_dataset
 import json
 
+def change_format(convs):
+    for conv in convs:
+        if conv['role']=='user':
+            conv['from'] = 'human'
+        elif conv['role'] == 'assistant':
+            conv['from'] = 'gpt'
+        conv.pop('role')
+        conv['value'] = conv.pop('content')
+
 # 加载数据集
 ds = load_dataset("HuggingFaceH4/ultrafeedback_binarized", split='train_sft')
 
@@ -8,6 +17,8 @@ ds = load_dataset("HuggingFaceH4/ultrafeedback_binarized", split='train_sft')
 modified_data = []
 for example in ds:
     example['conversations'] = example.pop('messages')
+    change_format(example['chosen'])
+    change_format(example['rejected'])
     modified_data.append(example)
 
 # 将修改后的数据集保存为 JSON 文件
