5e8d2170837620d4aa0a68307206df892f873650
luxinyu <181303216@yzu.edu.cn>
2024-09-05 21:35:34 +0800

    update for spin

---
+++ algorithms/spin/inference_for_spin.sh
@@ -0,0 +1,6 @@
+autoalign-cli infer --backend "vllm" \
+            --model-name ${MODEL_NAME} \
+            --model-path ${SAVED_MODEL_PATH} \
+            --test-file ${PROMPTS_FILE} \
+            --template ${TEMPLATE_NAME} \
+            --source ${REJECTED_SOURCE_TAG}
