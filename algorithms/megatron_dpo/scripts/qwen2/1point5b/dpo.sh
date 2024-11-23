a7d77e120a31095b4bdd321b4b377f3b7679aab2
zhangqingyu <972437823@qq.com>
2024-11-24 00:45:14 +0800

    test qwen2.5 dpo scuessfully

--- algorithms/megatron_dpo/scripts/qwen2/1point5b/dpo.sh
+++ algorithms/megatron_dpo/scripts/qwen2/1point5b/dpo.sh
@@ -1,4 +1,4 @@
-cd /ciphome/zhangqingyu2023/code/auto-alignment/algorithms/megatron_dpo/examples/qwen2
+cd /share/zhangqingyu/code/auto-alignment/algorithms/megatron_dpo/examples/qwen2
 sh run_mcore_qwen_dpo.sh  \
 dsw  \
 1.5B   \
@@ -20,9 +20,9 @@ ture \
 false   \
 false \
 100000  \
-/ciphome/zhangqingyu2023/data/dpo/ultrafeedback_binarized_conversations_maxlen_2048   \
-/ciphome/zhangqingyu2023/data/dpo/ultrafeedback_binarized_conversations_maxlen_2048  \
-/ciphome/zhangqingyu2023/mg_models/Qwen2.5-1.5B-hf-to-mcore-te-tp2-pp2 \
+/share/zhangqingyu/data/dpo/ultrafeedback_binarized_conversations_maxlen_2048   \
+/share/zhangqingyu/data/dpo/ultrafeedback_binarized_conversations_maxlen_2048  \
+/share/zhangqingyu/mg_models/Qwen2.5-1.5B-hf-to-mcore-te-tp2-pp2 \
 1000000000  \
 100   \
-/ciphome/zhangqingyu2023/mg_models/Qwen2.5-1.5B-hf-to-mcore-te-tp2-pp2
+/share/zhangqingyu/mg_models/Qwen2.5-1.5B-hf-to-mcore-te-tp2-pp2
