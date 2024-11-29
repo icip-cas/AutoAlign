c5f0697bfdcee371c581b394e8d4c790681efcbd
zhangqingyu <972437823@qq.com>
2024-11-29 21:14:52 +0800

    clean some scripts

--- src/autoalign/train_megatron/examples/qwen2_5/preprocess_data_sft_conv.sh
+++
@@ -1,9 +0,0 @@
-export PYTHONPATH=$(dirname $(dirname $(pwd))):$PYTHONPATH
-cd toolkits/sft_conv_data_preprocessing
-bash run_build_idxmap_sft_conv_dataset.sh \
-/mnt/userdata/data6/xudong2022/data/InfInstruct-Gen/infinite_9m.json \
-conversations \
-Qwen2Tokenizer \
-4096 \
-/ciphome/zhangqingyu2023/data/sft/InfInstruct-Gen_infinite_9m \
-/mnt/userdata/hf_models/qwen/Qwen2.5-7B
--- src/autoalign/train_megatron/scripts/qwen2/1point5b/preprocess_data_sft.sh
+++
@@ -1,7 +0,0 @@
-cd /ciphome/zhangqingyu2023/code/Pai-Megatron-Patch/toolkits/sft_data_preprocessing
-bash run_build_idxmap_sft_dataset.sh \
-/ciphome/zhangqingyu2023/code/Pai-Megatron-Patch/data/qwen-datasets/qwen_sft.json \
-Qwen2Tokenizer \
-2048 \
-/ciphome/zhangqingyu2023/code/Pai-Megatron-Patch/data/qwen-datasets/mmap_qwen2_sft_datasets \
-/ciphome/zhangqingyu2023/hf_models/Qwen2-1.5B
