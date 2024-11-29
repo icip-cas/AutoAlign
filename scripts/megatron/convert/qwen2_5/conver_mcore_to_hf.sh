c29e99ffc6cf164b2d101142077f21bd1f28c3cb
zhangqingyu <972437823@qq.com>
2024-11-29 20:37:44 +0800

    seperate convert.sh to scripts/megatron/convert

---
+++ scripts/megatron/convert/qwen2_5/conver_mcore_to_hf.sh
@@ -0,0 +1,12 @@
+export CUDA_VISIBLE_DEVICES=7
+cd ../../src/autoalign/train_megatron/toolkits/model_checkpoints_convertor/qwen
+bash hf2mcore_qwen2.5_convertor.sh \
+7B \
+/ciphome/zhangqingyu2023/checkpoint/sft/Qwen2.5-7B-hf-to-mcore-te-tp2-pp1/checkpoint/sft-mcore-qwen2_5-7B-lr-5e-6-minlr-0.0-bs-4-gbs-512-seqlen-4096-pr-bf16-tp-2-pp-1-cp-1-ac-none-do-true-sp-true-ti-10000-wi- \
+/ciphome/zhangqingyu2023/hf_models/Qwen2.5-7B-tp2-pp1-sft-checkpoint5000-infinite_9m \
+2  \
+1  \
+fp32 \
+true \
+true \
+/mnt/userdata/hf_models/qwen/Qwen2.5-7B
