424b09fff3c2561cc71fb08ed0a0e112282f4295
zhangqingyu <972437823@qq.com>
2024-12-09 00:47:36 +0800

    modify conver scripts

--- scripts/megatron/convert/qwen2_5/conver_mcore_to_hf.sh
+++ scripts/megatron/convert/qwen2_5/conver_mcore_to_hf.sh
@@ -1,12 +1,26 @@
-export CUDA_VISIBLE_DEVICES=7
-cd ../../src/autoalign/train_megatron/toolkits/model_checkpoints_convertor/qwen
+#!/bin/bash
+export CUDA_VISIBLE_DEVICES=${CUDA_VISIBLE_DEVICES:-7}
+ROOT=${ROOT:-"../../../autoalign"}
+
+MODEL_SIZE=${MODEL_SIZE:-"7B"}
+TP=${TP:-"2"}
+PP=${PP:-"2"}
+MG_MODEL_PATH=${MG_MODEL_PATH:-"${ROOT}/mg_models/Qwen2.5-${MODEL_SIZE}-hf-to-mcore-te-tp${TP}-pp${PP}"}
+HF_MODEL_PATH=${HF_MODEL_PATH:-"${ROOT}/hf_models/Qwen2.5-${MODEL_SIZE}-mcore-to-hf-tp${TP}-pp${PP}"}
+PRECISION=${PRECISION:-"fp32"}
+USE_TE=${USE_TE:-"true"}
+MG2HF=${MG2HF:-"true"}
+HF_MODEL_SOURCE=${HF_MODEL_SOURCE:-"${ROOT}/hf_models/Qwen2.5-${MODEL_SIZE}"}
+
+cd ${ROOT}/src/autoalign/train_megatron/toolkits/model_checkpoints_convertor/qwen
+
 bash hf2mcore_qwen2.5_convertor.sh \
-7B \
-/ciphome/zhangqingyu2023/mg_models/Qwen2.5-7B-hf-to-mcore-te-tp2-pp2 \
-/ciphome/zhangqingyu2023/hf_models/Qwen2.5-7B-mcore-to-hf-tp2-pp2-test \
-2  \
-2  \
-fp32 \
-true \
-true \
-/mnt/userdata/hf_models/qwen/Qwen2.5-7B
+    ${MODEL_SIZE} \
+    ${MG_MODEL_PATH} \
+    ${HF_MODEL_PATH} \
+    ${TP} \
+    ${PP} \
+    ${PRECISION} \
+    ${USE_TE} \
+    ${MG2HF} \
+    ${HF_MODEL_SOURCE}
