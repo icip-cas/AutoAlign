aef7a2d0703a4a417023511a49b2721f4632e145
xudong2022 <xudong2022@iscas.ac.cn>
2024-12-16 03:08:37 +0800

    self-rewarding finish readme and scripts

--- algorithms/self-rewarding/scripts/dpo_data_gen_iter2.sh
+++ algorithms/self-rewarding/scripts/dpo_data_gen_iter2.sh
@@ -1,8 +1,8 @@
 python src/dpo_dataset_generator.py \
-    --model-path ${MODEL_PATH:-"saved_models/llama3-8b-eft"} \
+    --model-path ${MODEL_PATH:-"saved_models/llama3-8b-eft-dpo-iter1"} \
     --model-id ${MODEL_ID:-"llama3-8b"} \
     --template-name ${TEMPLATE_NAME:-"llama-3-instruct"} \
-    --question-gen-model-path ${QGEN_MODEL:-"/mnt/userdata/hf_models/NousResearch/Meta-Llama-3-8B-Instruct"} \
+    --question-gen-model-path ${QGEN_MODEL:-"meta-llama/Meta-Llama-3-8B-Instruct"} \
     --seed-data-path ${SEED_DATA_PATH:-"data/seed.json"} \
     --sft-base-model ${SFT_BASE_MODEL:-"eft"} \
     --backend ${BACKEND:-"vllm"} \
