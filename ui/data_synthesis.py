33bce3ced63b3f2b31fa75c9371c4dc3d2880fe4
Maoyingzhi <maoyingzhi2024@iscas.ac.cn>
2025-03-04 14:17:04 +0800

    Data synthesis and eval

---
+++ ui/data_synthesis.py
@@ -0,0 +1,50 @@
+import streamlit as st
+
+st.title("Data Synthesis")
+
+with st.form("config_form"):
+    col1, col2 = st.columns([2, 3])
+    with col1:
+        process = st.selectbox("Synthesis Method", ["Magpie","Self-instruction", "Instruction Backtranslation"])
+    
+    
+    st.subheader("Path Selection")
+    data_source = st.text_input("Input Dir", placeholder="Please provide the path for the data.")
+
+    model_dir = st.text_input("Model Dir", placeholder = "Please provide the path for the model.")
+
+    output_dir = st.text_input("Output Dir", placeholder="Please specify the path for saving the results.")
+
+    st.subheader("Parameters")
+    cols = st.columns(3)
+    with cols[0]:
+        project_name = st.number_input("Temperature", min_value = 0.0, max_value = 1.0, value = 0.8, step = 0.1)
+    with cols[1]:
+        iteration = st.number_input("Iteration Rounds", min_value=1, max_value=100, value=10)
+    with cols[2]:
+        temperature = st.number_input("Temperature", min_value=0.0, max_value=2.0, value=0.7, step=0.1)
+
+  
+    submitted = st.form_submit_button("Submit")
+
+    if submitted:
+        all_fields_filled = True
+        if not data_source:
+            st.error("Please provide the input directory.")
+            all_fields_filled = False
+
+        if not model_dir:
+            st.error("Please provide the model directory.")
+            all_fields_filled = False
+
+        if not output_dir:
+            st.error("Please specify the output directory.")
+            all_fields_filled = False
+
+        if not project_name:
+            st.error("Please provide the project name.")
+            all_fields_filled = False
+
+        if all_fields_filled:
+            st.switch_page("page2.py")
+            
---
+++ ui/eval.py
@@ -0,0 +1,44 @@
+import streamlit as st
+
+st.title("Model Eval")
+
+with st.form("config_form"):
+    st.subheader("BenchMark")
+    col1, col2 = st.columns([3,2])
+    with col1:
+        process = st.selectbox(
+            "BenchMark", 
+            [
+            "1",
+            "2",
+            "3"
+            ],
+            label_visibility="collapsed"
+        )
+    
+    st.subheader("Model Selection")
+
+    model_dir = st.text_input("Model Dir", placeholder = "Please provide the path for the model.", label_visibility="collapsed")
+
+    st.subheader("Results")
+
+    output_dir = st.text_input("Result Dir", placeholder="Please specify the path for saving the results.", label_visibility="collapsed")
+
+    col1, col2, col3  = st.columns([4, 2, 4])
+    with col2:
+        submitted = st.form_submit_button("Start")
+
+    if submitted:
+        all_fields_filled = True
+
+        if not model_dir:
+            st.error("Please provide the model directory.")
+            all_fields_filled = False
+
+        if not output_dir:
+            st.error("Please specify the output directory.")
+            all_fields_filled = False
+
+        if all_fields_filled:
+            st.switch_page("page5.py")
+        
