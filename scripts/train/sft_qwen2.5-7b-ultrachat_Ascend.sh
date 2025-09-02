8a27f636a12ecda39dc3d3f34b638be4df735084
SsyllZz <865408929@qq.com>
2025-09-02 15:22:58 +0800

    0902-v4

--- scripts/train/sft_qwen2.5-7b-ultrachat_Ascend.sh
+++ scripts/train/sft_qwen2.5-7b-ultrachat_Ascend.sh
@@ -1,11 +1,11 @@
 export DATA_PATH="data/dummy_sft.json"
 # export CONV_TEMPLATE=chatml-keep-system
-export OUTPUT_DIR="/mnt/data3/maoyingzhi2024/ATA-Ascend/AutoAlign/model/"
-export MODEL_PATH="/mnt/data1/hf_models/Qwen2.5-1.5B"
+export OUTPUT_DIR="output/model/qwen2.5-7b"
+export MODEL_PATH="model/Qwen2.5-7B"
 export GA=32
 export DS_CONFIG="configs/zero3.json"
-export TRAIN_BATCH_SIZE=1
+export TRAIN_BATCH_SIZE=2
 export logging_step=1
-export MAX_LENGTH=1024
+export MAX_LENGTH=4096
 bash scripts/train_sft.sh
 
